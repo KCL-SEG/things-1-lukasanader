3ad210a36771a7210b60bf477b8627d9ac0489b3
lukasanader <lukasanader@hotmail.com>
2023-11-03 17:12:55 +0000

    thungs

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -124,6 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#User model
-AUTH_USER_MODEL = 'things.Thing'
