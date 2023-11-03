411e1fbb1d3069e5f9435cfefceae2d8b61105c1
lukasanader <lukasanader@hotmail.com>
2023-11-03 17:15:26 +0000

    things

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import django.core.validators
 
 # Create your models here.
-class Thing():
+class Thing(models):
     name= models.CharField(unique=True,max_length=30,blank=False)
     description = models.TextField(max_length=120,blank=True)
     quantity = models.IntegerField(
